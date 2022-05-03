d393fd69aa276818e0312c84f70fe4fde1e8aadf
daniel0612 <goodluck1031@outlook.com>
2022-05-03 06:50:22 -0700

    create bar3D and column3D charts

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -43,7 +43,18 @@ const Repos = () => {
     })
     .slice(0, 5)
 
-  console.log(mostPopular)
+  // stars, forks
+  let {stars, forks} = repos.reduce((total, item) => {
+    const {stargazers_count, name, forks} = item
+    total.stars[stargazers_count] = {label:name, value: stargazers_count}
+    total.forks[forks] = {label: name, value: forks}
+    return total
+  }, {
+    stars: {}, forks: {}
+  })
+  
+  stars = Object.values(stars).slice(-5).reverse()
+  forks = Object.values(forks).slice(-5).reverse()
 
   const chartData = [
     {
@@ -64,9 +75,9 @@ const Repos = () => {
     <section className="section">
       <Wrapper className="section-center">
         <Pie3D data={mostUsed}/>
-        <Column3D data={chartData} />
+        <Column3D data={stars} />
         <Doughnut2D data={mostPopular}/>
-        <Bar3D data={chartData} />
+        <Bar3D data={forks} />
       </Wrapper>
     </section>
   )
