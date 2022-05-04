30d62545c7812771c329a95d13c0a18ebbdaf6be
daniel0612 <goodluck1031@outlook.com>
2022-05-04 10:30:45 -0700

    add login, logout using auth0.com

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
-
+//dev-bn7fqrvw.us.auth0.com
+//p7HEDcpFMfA9w7T2FeIeDiYxRvHQgo7Y
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain="dev-bn7fqrvw.us.auth0.com"
+      clientId="p7HEDcpFMfA9w7T2FeIeDiYxRvHQgo7Y"
+      redirectUri={window.location.origin} 
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
